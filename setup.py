ed6a24ad73714223fa30758a11ad1167e80a4fc6
Ctrl-alt-delet <AutoPatya@yandex.ru>
2024-11-19 18:15:21 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
-
+setup(
+ name="MyFirstTestBot",
+ version="1.0.0",
+ packages=find_packages(),
+ install_requires=
+ "requests",
+ "flask",
+,
+ entry_points={
+ "console_scripts": 
+ "mftb = mftb:app",
+ 
+ }
+)
